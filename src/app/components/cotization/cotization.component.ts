01060a0d205013b4c9bc6ee62f5af1d2c054e648
Luis._ <luis21080388mr@gmail.com>
2023-06-11 16:46:15 -0500

    config

--- src/app/components/cotization/cotization.component.ts
+++ src/app/components/cotization/cotization.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Client } from 'src/app/models/Client';
@@ -11,14 +12,67 @@ import { ClientService } from 'src/app/services/client.service';
 export class CotizationComponent implements OnInit {
 
   id!: number;
+
+  myForm!: FormGroup;
+
+
+  caps = ["Mensual", "Bimestral", "Trimestral", "Cuatrimestral", "Semestral", "Anual"];
+  gracias = ["Total", "Parcial"];
+
+
+
   constructor(private clientservice: ClientService,
               private activatedRouter:ActivatedRoute,
-              private route: Router) { }
+              private route: Router,
+              private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
     this.id= this.activatedRouter.snapshot.params["id"];
     this.loadUser();
+    this.loadform();
   }
+
+
+  loadform(){
+    this.myForm = this.formBuilder.group({
+      capital: ["", [Validators.required]],
+      gracia: ["", [Validators.required]]
+    })
+  }
+
+  tipotasa!: string;
+  checkradio(event: any){
+    console.log(event.value);
+    this.tipotasa = event.value;
+    console.log(this.tipotasa);
+  }
+
+  moneda!: string;
+  checkmoneda(event: any){
+    console.log(event.value);
+    this.moneda = event.value;
+    console.log(this.moneda);
+  }
+
+  continue(){
+
+
+    let tasa = this.tipotasa;
+    let capital = this.myForm.get('capital')!.value;
+    let gracia = this.myForm.get('gracia')!.value;
+    let moneda = this.moneda;
+    console.log(2);
+    console.log(tasa);
+    console.log(capital);
+    console.log(gracia);
+    console.log(moneda);
+
+
+  }
+
+
+
+
 
   usernow!:Client;
   loadUser()
